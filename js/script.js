eb933c8934c4145db7485c2a4e2384cb3aef4892
gitmaciejpiotr <97894371+gitmaciejpiotr@users.noreply.github.com>
2023-11-02 15:01:12 +0100

    Add adding active class to article

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ const titleClickHandler = function (event) {
     console.log('Link was clicked!');
     console.log(event);
 
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
 
     const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -17,15 +17,21 @@ const titleClickHandler = function (event) {
         activeLink.classList.remove('active');
     }
 
-    /* add class 'active' to the clicked link */
+    /* [IN PROGRESS] add class 'active' to the clicked link */
+
+    const clickedElement = this;
+
+    console.log('clickedElement:', clickedElement);
+
+    clickedElement.classList.add('active');
+
+    /* [DONE] remove class 'active' from all articles */
 
     const activeArticles  = document.querySelectorAll('.posts article.active');
 
     for (let activeArticle of activeArticles ) {
         activeArticle.classList.remove('active');
     }
-
-    /* remove class 'active' from all articles */
 
     /* get 'href' attribute from the clicked link */
 
